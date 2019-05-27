91741c2d3219a71196c9ecf9acc7ca7b43066d64
Colores1976 <43757999+Colores1976@users.noreply.github.com>
2019-05-27 20:03:33 +0200

    27.05.update

--- kodilla-stream/kodilla-exception/src/main/java/com/kodilla/exeption/test/FirstChallenge.java
+++ kodilla-stream/kodilla-exception/src/main/java/com/kodilla/exeption/test/FirstChallenge.java
@@ -2,24 +2,29 @@ package com.kodilla.exeption.test;
 
 import java.io.IOException;
 
-public class FirstChallenge {
 
-    public double divide(double a, double b) {
+    public class FirstChallenge {
 
-       try {
-        } catch (ArithmeticException e) {
-        System.out.println("Czemu to się nie drukuje" + e);
-        } finally {
-            System.out.println("Dziękujemy za skorzystanie z kalkulatora");
+        public double divide(double a, double b) throws ArithmeticException {
+            if(b == 0){
+                throw new ArithmeticException();
+            }
+            return a / b;
         }
-    return a/b;
-    }
 
+        public static void main(String[] args) {
 
-    public static void main(String[] args){
-        FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-        System.out.println();
-        System.out.println(result);
+            FirstChallenge firstChallenge = new FirstChallenge();
+            double result = 0.0;
+            try {
+                result =  firstChallenge.divide(3, 0);
+            } catch (ArithmeticException e) {
+                System.out.println("Czemu to się nie drukuje" + e);
+            } finally {
+                System.out.println("Dziękujemy za skorzystanie z kalkulatora");
+            }
+
+            System.out.println(result);
+
+        }
     }
-}
